f673c5b0cddd6a0e165a8b368f7b45336fb30caf
FmG <g@example.com>
2024-05-15 19:49:30 +0200

    added OU

--- main.tf
+++ main.tf
@@ -1,17 +1,22 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 terraform {
-    backend "s3" {
-        bucket = "harkom-terraform-state"
-        key    = "terraform.tfstate"
-        region = "us-east-1"
-        dynamodb_table = "harkom-terraform-state-lock"
-    }
+  backend "s3" {
+    bucket         = "harkom-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "harkom-terraform-state-lock"
+  }
 }
 
 resource "aws_organizations_organization" "harkom" {
-    aws_service_access_principals = ["config-multiaccountsetup.amazonaws.com"]
-    feature_set                   = "ALL"
+  aws_service_access_principals = ["config-multiaccountsetup.amazonaws.com"]
+  feature_set                   = "ALL"
+}
+
+resource "aws_organizations_organizational_unit" "prod" {
+  name      = "Prod"
+  parent_id = aws_organizations_organization.harkom.roots[0].id
 }
